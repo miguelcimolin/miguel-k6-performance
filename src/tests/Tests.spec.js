6a9c6404b2ac424f9ed16adf1e18549cf4a8d43f
Miguel <69534716+miguelcimolin@users.noreply.github.com>
2024-11-27 20:29:15 -0300

    Update Tests.spec.js

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,46 +4,53 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+export const getContactsDuration = new Trend('get_contacts', true); //trend no get
+export const RateContentOK = new Rate('content_OK'); // rate no status code
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_failed: ['rate<0.12'], // 12% de margem
+    get_contacts: ['p(95)<5700'], // 5700 aqui
+    content_OK: ['rate>0.95'], // 95% das respostas tem que dar certo
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
-  ]
+    { duration: '10s', target: 10 },
+    { duration: '50s', target: 10 }, // 1m
+    { duration: '10s', target: 60 },
+    { duration: '50s', target: 60 }, // 2m
+    { duration: '10s', target: 100 },
+    { duration: '40s', target: 100 },
+    { duration: '10s', target: 180 }, // 3m
+    { duration: '40s', target: 180 },
+    { duration: '20s', target: 300 }, // 4m
+    { duration: '60s', target: 300 }, // 5m
+  ],
 };
 
 export function handleSummary(data) {
   return {
     './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
   };
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://api.thecatapi.com/v1/images/search';
 
   const params = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+      "x-api-key": "live_HKm2yfEB9OLkeFE7wpts2nuAXHtMzfNNNCIsMf5RBfA2MHKy6y3xats4YeRTefFI"
+    },
   };
 
-  const OK = 200;
+  const response = http.get(baseUrl, params);
 
-  const res = http.get(`${baseUrl}`, params);
+  getContactsDuration.add(response.timings.duration);
 
-  getContactsDuration.add(res.timings.duration);
+  RateContentOK.add(response.status === 200);
 
-  RateContentOK.add(res.status === OK);
-
-  check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+  check(response, {
+    'GET Gato - Status 200': () => response.status === 200,
   });
 }
